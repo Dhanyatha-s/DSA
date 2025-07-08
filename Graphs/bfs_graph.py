b73fee8a8f65b71cb8b5de755aa1d272ba60e186
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2025-07-08 23:04:30 +0530

    Update bfs_graph.py

--- Graphs/bfs_graph.py
+++ Graphs/bfs_graph.py
@@ -1,1 +1,15 @@
+from collections import deque
 
+def bfs(start):
+  queue = deque([start])
+  visisted.add(start)
+
+  while queue:
+    node = graph.popleft()
+    print(node, end='')
+
+    for neighbor in graph[node]:
+      if neighbor not in visited:
+        queue.add(neighbor)
+        visited.add(neighbor)
+bfs("A")

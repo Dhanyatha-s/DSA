422e8433935108e460e9b25dd9dd1578df050a03
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2025-07-08 21:02:53 +0530

    Update dfs_graph.py

--- Graphs/dfs_graph.py
+++ Graphs/dfs_graph.py
@@ -1,1 +1,30 @@
+from collections import defaultdict
+# defaultdict , add neighbours even if its not yet added
+
+graph = defaultdict(list)
+
+def add_edge(u,v):
+  graph[u].append(v)
+  graph[v].append(u)   # neighbours
+
+# Add all the connections
+add_edge('A', 'B')
+add_edge('A', 'C')
+add_edge('B', 'D')
+add_edge('C', 'E')
+
+# DFS
+visited = ()  # set
+def dfs(node):
+ 
+  if node not n visited:
+    print(node, end='')
+    visited.add(node)
+
+    for neighbor in graph[node]:
+      dfs(neighbor)
+
+dfs('A')
+
+
 

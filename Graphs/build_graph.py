8e6d5c58c195453e589bb6298d2ae0d9b9c7389e
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2025-07-08 20:44:06 +0530

    Update build_graph.py

--- Graphs/build_graph.py
+++ Graphs/build_graph.py
@@ -1,1 +1,13 @@
+def build_graph(edges):
+  graph = {}
+  for u,v in edges:
+    graph.setdefault(u,[]).append(v)
+    graph.setdefault(v,[]).append(u)
 
+  return graph
+
+graph = build_graph(edges)
+print(graph)
+
+
+edges = [(0,1), (0,3), (1,2), (2,4), (4,5)
